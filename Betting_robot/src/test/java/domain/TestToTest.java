2453f0b45dee05faa3baaa7647808cf3f3f52223
dooomdo <dooomdo@net.hr>
2014-07-24 18:03:46 +0200

    test commit

--- Betting_robot/src/test/java/domain/TestToTest.java
+++ Betting_robot/src/test/java/domain/TestToTest.java
@@ -10,7 +10,9 @@ public class TestToTest {
 	@Test
 	public void testTest() {
 		String variable="1";
+		String variable2="2";
 		assertEquals(variable,"1");
+		assertEquals(variable2,"2");
 	}
 
 }
